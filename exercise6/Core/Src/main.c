1e4927576f0736df3ee9a955fb262c528585881c
mtuan8820 <mtuan9517@gmail.com>
2022-10-08 08:25:30 +0700

    lab1_ex9 add clearNumberOnClock function

--- exercise6/Core/Src/main.c
+++ exercise6/Core/Src/main.c
@@ -80,6 +80,22 @@ void setNumberOnClock(int num){
 		if (num==11) {HAL_GPIO_WritePin(LED11_GPIO_Port, LED11_Pin, SET); state[num]=1;}
 		if (num==12) {HAL_GPIO_WritePin(LED12_GPIO_Port, LED12_Pin, SET); state[num]=1;}
 }
+void clearNumberOnClock(int num){
+	if (num<=0 || num >=13) return;
+	if (state[num]==0) return;
+	if (num==1) {HAL_GPIO_WritePin(LED1_GPIO_Port, LED1_Pin, RESET); state[num]=0;}
+	if (num==2) {HAL_GPIO_WritePin(LED2_GPIO_Port, LED2_Pin, RESET); state[num]=0;}
+	if (num==3) {HAL_GPIO_WritePin(LED3_GPIO_Port, LED3_Pin, RESET); state[num]=0;}
+	if (num==4) {HAL_GPIO_WritePin(LED4_GPIO_Port, LED4_Pin, RESET); state[num]=0;}
+	if (num==5) {HAL_GPIO_WritePin(LED5_GPIO_Port, LED5_Pin, RESET); state[num]=0;}
+	if (num==6) {HAL_GPIO_WritePin(LED6_GPIO_Port, LED6_Pin, RESET); state[num]=0;}
+	if (num==7) {HAL_GPIO_WritePin(LED7_GPIO_Port, LED7_Pin, RESET); state[num]=0;}
+	if (num==8) {HAL_GPIO_WritePin(LED8_GPIO_Port, LED8_Pin, RESET); state[num]=0;}
+	if (num==9) {HAL_GPIO_WritePin(LED9_GPIO_Port, LED9_Pin, RESET); state[num]=0;}
+	if (num==10) {HAL_GPIO_WritePin(LED10_GPIO_Port, LED10_Pin, RESET); state[num]=0;}
+	if (num==11) {HAL_GPIO_WritePin(LED11_GPIO_Port, LED11_Pin, RESET); state[num]=0;}
+	if (num==12) {HAL_GPIO_WritePin(LED12_GPIO_Port, LED12_Pin, RESET); state[num]=0;}
+}
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
@@ -124,10 +140,10 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-	 //tat ta ca den
-	 clearAllClock();
-	 //dem thoi gian
-	 if (status<=12) setNumberOnClock(status);
+
+	 for (int n=1;n<13;n++)setNumberOnClock(n);//bat tat ca den
+	 //test thu chuc nang cua clearNumberOnClock
+	 if (status<=12) clearNumberOnClock(status);
 	 status++;
 	 if(status>=13) status=1;
 	 ///////////////
